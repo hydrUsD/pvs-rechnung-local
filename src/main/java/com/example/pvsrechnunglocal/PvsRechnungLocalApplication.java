86c410085ea1f5fcb9b7deec4154409c177ab4b7
hydrUsD <proletsfail@gmail.com>
2025-04-05 13:44:33 +0200

    Basic project setup

---
+++ src/main/java/com/example/pvsrechnunglocal/PvsRechnungLocalApplication.java
@@ -0,0 +1,13 @@
+package com.example.pvsrechnunglocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvsRechnungLocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PvsRechnungLocalApplication.class, args);
+    }
+
+}
